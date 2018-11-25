348c0582a99295128cd2b8f7e2bfed756e720bc2
Rima Jean <rima.jean@gmail.com>
2018-11-25 17:46:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,18 +1,12 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
   get '/newteam' do
     erb :newteam
   end
 
 	post '/team' do
-    # <p>Team Name:<input type="text" name="name"></p>
-    # <p>Coach:<input type="text" name="coach"></p>
-    # <p>Point Guard:<input type="text" name="pg"></p>
-    # <p>Shooting Guard:<input type="text" name="sg"></p>
-    # <p>Small Forward:<input type="text" name="sf"></p>
-    # <p>Power Forward:<input type="text" name="pf"></p>
-    # <p>Center:<input type="text" name="c"></p>
     @name = params["name"]
     @coach = params["coach"]
     @point_guard = params["pg"]
